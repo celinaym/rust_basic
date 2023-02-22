3d4de322bbeee7caf81f474700c0a497b590d56f
celinaym <celinaym0810@gmail.com>
2023-02-22 14:43:00 +0900

    chapter3)function declaration

---
+++ src/function.rs
@@ -0,0 +1,14 @@
+fn add(num1:i32, num2:i32) -> i32{
+    return num1 + num2;
+}
+
+fn swap(num1:i32, num2:i32) -> (i32, i32){
+    (num2, num1)
+}
+
+pub fn function(){
+    println!("{}", add(1, 2));
+    //println!("num2={}, num1={}", swap(1, 2)); error
+    let (num1, num2) = swap(1,2);
+    println!("{num1}, {num2}")
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 mod variable;
 mod constant;
+mod function;
 
 fn main() {
     variable::variable();
     constant::constant();
+    function::function();
 }
